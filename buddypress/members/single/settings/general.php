3c85aa33b439ba7c49a6248e632d7322edcae21c
Jared Bennett <jarbenne@hwdsb.on.ca>
2020-01-16 09:29:55 -0500

    Initial Sync

---
+++ buddypress/members/single/settings/general.php
@@ -0,0 +1,8 @@
+<?php
+
+do_action( 'bp_before_member_settings_template' );
+
+do_action( 'bp_core_general_settings_before_submit' );
+do_action( 'bp_core_general_settings_after_submit' );
+
+do_action( 'bp_after_member_settings_template' );
